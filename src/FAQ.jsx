3672037a9921d072ed55a7515e5327c0f4d5c816
Lunepusa <92737802+Lunepusa@users.noreply.github.com>
2026-03-21 21:32:49 -0600

    Add usememo

--- src/FAQ.jsx
+++ src/FAQ.jsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from "react";
+import React, { StrictMode, usememo } from "react";
 import { createRoot } from "react-dom/client";
 import { reactDOM } from "react-dom";
 import "./styles.css";
