e0e3a0c69ba014622f98c4770079e5f8e91badda
Lunepusa <92737802+Lunepusa@users.noreply.github.com>
2026-05-31 23:14:50 -0600

    update src/Tags.jsx

--- src/Tags.jsx
+++ src/Tags.jsx
@@ -796,7 +796,7 @@ export const ClickableTags = ({ tags = "", emptyText = "None set" }) => {
   return (
     <div style={{ margin: "0.5% 0" }}>
       {tagArray.map((tag, i) => (
-        <pill key={tag} style{{padding:"0", margin:"2%", display:"inline-block"}}>
+        <pill key={tag} style={{padding:"0", margin:"2%", display:"inline-block"}}>
           <small>
           <a
             href={`/lounge#${encodeURIComponent(tag)}`}
