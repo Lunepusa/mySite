707db5f5dbfa3dc5d6ad2417c052d7828da476de
Lunepusa <92737802+Lunepusa@users.noreply.github.com>
2026-05-31 23:02:48 -0600

    update src/Tags.jsx

--- src/Tags.jsx
+++ src/Tags.jsx
@@ -814,7 +814,7 @@ export const ClickableTags = ({ tags = "", emptyText = "None set" }) => {
             {tag} ({counts[tag] || 0})
           </a></small>
           {i < tagArray.length - 1 && <small style={{ color: "#666" }}>, </small>}
-        </pillr>
+        </pill>
       ))}
     </div>
   );
