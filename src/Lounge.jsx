2f853c08398fa4a75d03b10b8dfb51be3f2c7dda
Lunepusa <92737802+Lunepusa@users.noreply.github.com>
2026-06-02 22:26:36 -0600

    update src/Lounge.jsx

--- src/Lounge.jsx
+++ src/Lounge.jsx
@@ -9,8 +9,8 @@ const Lounge = () => {
 
   return (
   <div  style={{ textAlign: "center" }}>
-    <h4 class="notice" style={{width:"100%"}}>
-    this is area is a work in progress, but has more of .y cobtent then anywhere else. send me a $10 tip and your username here and i will give you access for a month. </h4>,
+    <p class="notice" style={{width:"100%"}}>
+    this is area is a work in progress, but has more of .y cobtent then anywhere else. send me a $10 tip and your username here and i will give you access for a month. </p>,
     <p>
     {!user ? (
       <div style={{ fontSize: "1.3em", textAlign: "center" }}>
