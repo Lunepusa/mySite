c1997095c914836e74a6c6eb0b35c434d4ebf8ce
Lunepusa <92737802+Lunepusa@users.noreply.github.com>
2026-06-02 22:26:00 -0600

    update src/Lounge.jsx

--- src/Lounge.jsx
+++ src/Lounge.jsx
@@ -11,7 +11,7 @@ const Lounge = () => {
   <div  style={{ textAlign: "center" }}>
     <h4 class="notice" style={{width:"100%"}}>
     this is area is a work in progress, but has more of .y cobtent then anywhere else. send me a $10 tip and your username here and i will give you access for a month. </h4>,
-    
+    <p>
     {!user ? (
       <div style={{ fontSize: "1.3em", textAlign: "center" }}>
         This is a private area. Log in below and subscribe to remove the blurring.
@@ -35,7 +35,7 @@ const Lounge = () => {
             </Collapse>
       )}
        </div>
-    )}
+    )}</p>
       <Gallery />
     </div>
   );
