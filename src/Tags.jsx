06516200b856b9ff3d25b14b908fe34c27c240de
Lunepusa <92737802+Lunepusa@users.noreply.github.com>
2026-06-02 22:12:21 -0600

    update src/Tags.jsx

--- src/Tags.jsx
+++ src/Tags.jsx
@@ -794,7 +794,7 @@ export const ClickableTags = ({ tags = "", emptyText = "None set" }) => {
   }
 
   return (
-    <div style={{ margin: "0.5% 0" }}>
+    <div style={{ margin:"0%"}}>
       {tagArray.map((tag, i) => (
         <span key={tag} style={{padding:"0", margin:"2%", display:"inline-block"}}>
           <small>
